45cfced75e3f635b674787bb097d380f263c7402
postre554 <moglimon554@gmail.com>
2023-05-26 20:56:11 -0400

    config database2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '3.15.37.165', user: 'deploy', roles: %w{app db web}
+server '18.221.89.68', user: 'deploy', roles: %w{app db web}
 
 
 
