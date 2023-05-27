3dc8eb0115afd4b93d4e046619e06ad1314367f7
postre554 <moglimon554@gmail.com>
2023-05-27 13:33:45 -0400

    set render v1

---
+++ bin/render-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/end bash
+
+#exit on error
+
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+bundle exec rake db:seed
